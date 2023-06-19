2f52645e13033fea1a899303521eadb0f039e1ad
Skrigan <sk_dm-821@mail.ru>
2023-06-20 02:53:32 +0300

    feat: add hulf of fs functions

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import { mkdir, readdir, copyFile } from "node:fs/promises";
+import { fileURLToPath } from "url";
+import path from "node:path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+  const srcPath = path.join(__dirname, "files");
+  const destPath = path.join(__dirname, "files_copy");
+
+  try {
+    const files = await readdir(srcPath);
+    await mkdir(destPath);
+    for (const file of files) {
+      await copyFile(path.join(srcPath, file), path.join(destPath, file));
+    }
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import { rename as fsRename } from "node:fs/promises";
+import { fileURLToPath } from "url";
+import path from "node:path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    const dir = path.join(__dirname, "files");
+    await fsRename(path.join(dir, 'wrongFilename.txt'), path.join(dir, 'properFilename.md'))
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
