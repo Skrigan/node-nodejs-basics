1a70838479b92b1d3b21f35c19dae4c78c3d238c
Skrigan <sk_dm-821@mail.ru>
2023-06-22 21:40:25 +0300

    feat: add transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from "url";
+import path from "node:path";
+import { Transform } from "stream";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const transform = async () => {
-    // Write your code here 
+  const transform = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversed = chunk.toString().split("").reverse().join("") + "\n";
+      this.push(reversed);
+      callback();
+    },
+  });
+
+  process.stdin.pipe(transform).pipe(process.stdout);
 };
 
 await transform();
