ab9bd26078279ad950625e7ea528ac803a687d83
Skrigan <sk_dm-821@mail.ru>
2023-06-22 16:34:12 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createHash } from "node:crypto";
+import { readFile } from "node:fs/promises";
+import { fileURLToPath } from "url";
+import path from "node:path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const fileData = await readFile(
+      path.join(__dirname, "files/fileToCalculateHashFor.txt"), { encoding: "utf8" }
+    );
+    const hash = createHash("sha256").update(fileData).digest("hex");
+    console.log(hash);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await calculateHash();
