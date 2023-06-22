8a66f1611f44a7fe2f00e872ac8e6d1c742294de
Skrigan <sk_dm-821@mail.ru>
2023-06-22 16:00:34 +0300

    feat: add modules

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const res = [];
+  for (let i = 0; i < args.length; i = i + 2) {
+    res.push(`${args[i]} is ${args[i + 1]}`);
+  }
+  console.log(res.join(", "));
 };
-
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+  const filteredEnvEntries = Object.entries(process.env).filter((couple) => {
+    if (couple[0].slice(0, 4) === "RSS_") {
+      return true;
+    }
+    return false;
+  });
+  const res = [];
+  filteredEnvEntries.forEach((couple) => {
+    res.push(`${couple[0]}=${couple[1]}`);
+  });
+  console.log(res.join("; "));
 };
 
 parseEnv();
